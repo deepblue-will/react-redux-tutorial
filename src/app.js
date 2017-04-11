30cd7cddbafe6cd19a2cfd4b10725a5c2d47de63
deepblue_will <deepblue.will@gmail.com>
2017-04-11 23:56:38 +0900

    :+1: Apply react-dom

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import expect from 'expect'
 import { createStore } from 'redux'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// reducer
 const counter = (state = 0, action) => {
   switch(action.type) {
     case 'INCREMENT':
@@ -13,12 +14,37 @@ const counter = (state = 0, action) => {
   }
 }
 
+const Counter = ({
+  value,
+  onIncrement,
+  onDecrement
+  }) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+)
+
 //作成したreducerであるcounter関数を引数に指定してstoreを作成
 const store = createStore(counter)
 
 //画面更新用の関数を作成
 const render = () => {
-  document.body.innerText = store.getState()
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() =>
+        store.dispatch({
+          type: 'INCREMENT'
+        })}
+      onDecrement={() =>
+        store.dispatch({
+          type: 'DECREMENT'
+        })}
+    />,
+    document.getElementById('root')
+  )
 }
 
 //subscribe関数に、現在のstateの状況を画面に表示する関数をセット
@@ -26,9 +52,3 @@ store.subscribe(render)
 
 //最初に画面を表示（0が表示される)
 render()
-
-//documentオブジェクト(画面上すべて)にクリックしたらINCREMENTアクションをdispatchする
-//イベントを追加
-document.addEventListener('click', () => {
-  store.dispatch({ type: 'INCREMENT' })
-})
